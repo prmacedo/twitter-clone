c0fce2f0e65ac68ee0b65b76eea41c92ade202b0
prmacedo <p.ricardomacedo@hotmail.com>
2022-10-28 13:33:11 -0300

    create Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,28 @@
+import { Button } from "../Button/Button";
+import { Heading } from "../Heading/Heading";
+import { Text } from "../Text/Text";
+
+import './footer.css';
+
+export function Footer(){
+  return (
+    <footer className='w-full bg-blue-1 flex justify-center py-3 fixed bottom-0 left-0'>
+      <div className="footer__content flex items-center justify-between">
+        <div className="footer__text">
+          <Heading color="white">
+            Don't miss what is happening
+          </Heading>
+
+          <Text color="white" asChild>
+            <p>People on Twitter are the first to know.</p>
+          </Text>
+        </div>
+
+        <div className="footer__btnGroup grid grid-cols-2 gap-x-3">
+          <Button style="outline" color="white">Log in</Button>
+          <Button color="white">Sign Up</Button>
+        </div>
+      </div>
+    </footer>
+  )
+}
