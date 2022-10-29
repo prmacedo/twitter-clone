dd66a069f0b31aa1170a859be080649b6223a1a6
prmacedo <p.ricardomacedo@hotmail.com>
2022-10-29 16:19:02 -0300

    create News component

---
+++ src/components/News/News.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { News } from "./News";
+
+export default {
+  title: 'Components/News',
+  component: News
+} as Meta
+
+export const Default: StoryObj = {}
