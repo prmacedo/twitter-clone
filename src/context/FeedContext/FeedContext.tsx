202d3388bab5769a75bbd13d95776e5183f705f6
prmacedo <p.ricardomacedo@hotmail.com>
2022-10-31 16:08:16 -0300

    create feed context

---
+++ src/context/FeedContext/FeedContext.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { TweetProps } from '../../components/Tweet/Tweet';
+
+interface FeedContextProps {
+  tweets: TweetProps[];
+  setTweets: React.Dispatch<React.SetStateAction<TweetProps[]>>;
+}
+
+interface FeedContextProviderProps {
+  children: ReactNode;
+}
+
+const FeedContext = createContext<FeedContextProps>({
+  tweets: [],
+  setTweets: () => {}
+});
+
+export function FeedContextProvider({ children }: FeedContextProviderProps) {
+  const [tweets, setTweets] = useState<Array <TweetProps>>(JSON.parse(String(localStorage.getItem("tweets"))));
+
+  useEffect(() => {
+    setTweets(JSON.parse(String(localStorage.getItem("tweets"))))
+  }, []);
+
+  return (
+    <FeedContext.Provider
+      value={{
+        tweets,
+        setTweets
+      }}
+    >
+      {children}
+    </FeedContext.Provider>
+  );
+}
+
+export function useFeed() {
+  const context = useContext(FeedContext);
+  const { tweets, setTweets } = context;
+  return { tweets, setTweets };
+}
