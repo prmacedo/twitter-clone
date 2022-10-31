0ebbb653d00b876fa4884759d883d35dff3277d5
prmacedo <p.ricardomacedo@hotmail.com>
2022-10-31 11:13:57 -0300

    fix hover effect

--- src/components/ShortProfile/ShortProfile.tsx
+++ src/components/ShortProfile/ShortProfile.tsx
@@ -18,7 +18,7 @@ export function ShortProfile({ profilePic }: ShortProfileProps){
       <Text color="gray" size="lg" className="leading-4">@biscuttu</Text>
     </div>
 
-    <Icon icon="ellipses" className="justify-self-end hover:bg-dark-7 rounded-full p-2 box-content cursor-pointer" />
+    <Icon icon="ellipses" className="justify-self-end hover:bg-dark-7 dark:hover:bg-dark-4 rounded-full p-2 box-content cursor-pointer" />
    </div>
   )
 }
