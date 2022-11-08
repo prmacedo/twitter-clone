0099c319dd6317c5ae751ecdf51c5edd3d45d14c
prmacedo <p.ricardomacedo@hotmail.com>
2022-11-08 17:32:12 -0300

    create button variant

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ export interface ButtonProps {
   children: ReactNode;
   asChild?: boolean;
   style?: 'fill' | 'outline';
-  color?: 'blue' | 'white';
+  color?: 'blue' | 'white' | 'black';
   className?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
@@ -20,16 +20,18 @@ export function Button({size = 'default', children, asChild, style = 'fill', col
       onClick={onClick}
       className={
         clsx(
-          `font-sans font-bold text-base px-4 py-2.5 rounded-full duration-300 transition-colors ${className}`, {
+          `font-sans font-bold text-base px-4 py-2.5 rounded-full duration-300 transition-colors flex items-center justify-center gap-x-2 ${className}`, {
             'w-full': size !== 'default'
           },
           {
             'bg-blue-1 text-white hover:bg-blue-2': (color === 'blue' && style === 'fill'),
             'bg-white text-black hover:bg-dark-8': (color === 'white' && style === 'fill'),
+            'bg-black text-white hover:bg-black/90': (color === 'black' && style === 'fill'),
           },
           {
             'py-2 px-3.5 text-blue-1 border-2 border-blue-1 bg-transparent hover:bg-blue-1 hover:text-white': (color === 'blue' && style === 'outline'),
             'py-2 px-3.5 text-white border-2 border-white bg-transparent hover:bg-white/20': (color === 'white' && style === 'outline'),
+            'py-2 px-3.5 text-dark dark:text-white border-2 border-dark-7 bg-transparent hover:bg-dark-7/50 active:bg-dark-7/80': (color === 'black' && style === 'outline'),
           },
           {
             'py-1.5': (size === 'small'),
