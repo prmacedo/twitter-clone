e042449cc302aa0578c7378e547e99028f349f4f
prmacedo <p.ricardomacedo@hotmail.com>
2022-11-09 15:26:31 -0300

    refactor Tweet component, create TweetBase component and create comments funcionality

--- src/components/Tweet/Tweet.tsx
+++ src/components/Tweet/Tweet.tsx
@@ -1,19 +1,22 @@
-import { Heading } from "../Heading/Heading";
 import { Text } from "../Text/Text";
 import { Icon } from "../Icon/Icon";
 import { useEffect, useLayoutEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { useData } from "../../context/DataContext/DataContext";
 import { ITweets } from "../../types/ITweets";
 import { IUser } from "../../types/IUser";
 import { useUser } from "../../context/UserContext/UserContext";
 import { useFeed } from "../../context/FeedContext/FeedContext";
 
-import defaultPic from "../../imgs/default-profile-pic.svg";
+import { Post } from "../Post/Post";
+import { Modal } from "../Modal/Modal";
+import { Link } from "react-router-dom";
+import { TweetBase } from "./TweetBase/TweetBase";
 
-export function Tweet({ id, userId, time, description, img, likes, comments, retweets, share }: ITweets) {
+export function Tweet(tweet: ITweets) {
+  const { id, userId, time, description, img, likes, comments, retweets, share } = tweet;
   const [ likeStatus, setLikeStatus ] = useState<'wasLiked' | 'notLiked' | 'justLiked' | 'unliked'>();
   const [ liked, setLiked ] = useState<boolean>();
+  const [ showComments, setShowComments ] = useState(false);
 
   const { getUserById } = useData();
   const { user: loggedUser, isLoggedIn } = useUser();
@@ -30,26 +33,6 @@ export function Tweet({ id, userId, time, description, img, likes, comments, ret
     setLiked(!liked);
   }
 
-  function timeDiff(postTime: number) {
-    const now = new Date();
-    const seconds = (now.getTime() - postTime) / 1000;
-
-    let timeDiffString = '';
-
-    if (seconds < 1)
-      timeDiffString = 'now';
-    else if (seconds < 60)
-      timeDiffString = `${seconds.toFixed(0)}s`
-    else if (seconds < 3600)
-      timeDiffString = `${(seconds / 60).toFixed(0)}m`
-    else if (seconds < 3600 * 24)
-      timeDiffString = `${(seconds / (3600)).toFixed(0)}h`
-    else
-      timeDiffString = `${(seconds / (3600 * 24)).toFixed(0)}d`
-
-    return timeDiffString;
-  }
-
   function handleLike() {
     // If para funcionar no storybook
     if(!tweets) {
@@ -64,6 +47,12 @@ export function Tweet({ id, userId, time, description, img, likes, comments, ret
     }
   }
 
+  function handleComment() {
+    if (isLoggedIn) {
+      setShowComments(true);
+    }
+  }
+
   useLayoutEffect(() => {
     setUser(getUserById(userId));
 
@@ -74,9 +63,6 @@ export function Tweet({ id, userId, time, description, img, likes, comments, ret
       setLikeStatus('notLiked');
       setLiked(false);
     }
-
-    console.log(tweets);
-
   }, []);
 
   useEffect(() => {
@@ -109,28 +95,13 @@ export function Tweet({ id, userId, time, description, img, likes, comments, ret
   }, [likeStatus]);
 
   return (
-    <div className="tweet flex items-start gap-2.5 pl-4 py-2.5 border-t-2 border-dark-7 dark:border-dark-4">
-      <img src={ user?.profilePic || defaultPic } alt={ user?.name || 'Name' } className="w-12 rounded-full" />
-
-      <div className="feed__content pr-3 w-full">
-        <div className="feed__header flex gap-1 items-center mb-1">
-          <Heading size="xs">{ user?.name || 'Name' }</Heading>
-          <Text color="gray">@{ user?.user || 'username' }</Text>
-          <Text color="gray">·</Text>
-          <Text color="gray">{ timeDiff(time) } </Text>
-        </div>
-
-        <Text className="mb-2.5" asChild>
-          <p>{ description }</p>
-        </Text>
-
-        {
-          img && <img src={ img } alt={ description } className="w-full border-2 border-dark-7 dark:border-dark-4 rounded-2xl aspect-video object-cover" />
-        }
+    <>
+      <div className="pl-4 py-2.5 border-t-2 border-dark-7 dark:border-dark-4">
+        <TweetBase {...tweet} />
 
         <div className="feed__actions flex my-4">
           <div className="feed__iconGroup w-1/4">
-            <div className="cursor-pointer inline-flex gap-2.5">
+            <div className="cursor-pointer inline-flex gap-2.5"  onClick={() => handleComment()}>
               <Icon size="1.125rem" icon="comment" color="gray" />
               <Text size="xs" color="gray">{ comments.length }</Text>
             </div>
@@ -144,7 +115,7 @@ export function Tweet({ id, userId, time, description, img, likes, comments, ret
           </div>
 
           <div className="feed__iconGroup w-1/4">
-            <div className="cursor-pointer inline-flex gap-2.5"  onClick={() => handleLike()}>
+            <div className="cursor-pointer inline-flex gap-2.5" onClick={() => handleLike()}>
               <Icon size="1.125rem" icon='like' fill={ liked } color={ liked ? 'red' : 'gray' } />
               <Text size="xs" color={ liked ? 'red' : 'gray' }>{ likes.length }</Text>
             </div>
@@ -162,7 +133,28 @@ export function Tweet({ id, userId, time, description, img, likes, comments, ret
           <Link to={"not-found"}>Show this thread</Link>
         </Text>
       </div>
-    </div>
 
+      {
+        showComments &&
+        <Modal.Container>
+          <Modal.Content className="w-6/12 max-h-[90vh] overflow-y-auto scrollbar-none">
+            <Modal.CloseButton setIsOpen={setShowComments} />
+            <div id="comments">
+              <TweetBase {...tweet} isMainTweet />
+              {
+                tweets.map(tweet => {
+                  if(tweet.id === id) return tweet.comments.map(comment => <TweetBase key={comment.id} {...comment} isComment />)
+                })
+              }
+              <Post
+                id="postComment"
+                placeholder="Tweet your reply"
+                tweet={ tweet }
+              />
+            </div>
+          </Modal.Content>
+        </Modal.Container>
+      }
+    </>
   )
 }
---
+++ src/components/Tweet/TweetBase/TweetBase.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { TweetBase } from "./TweetBase";
+import { withRouter } from 'storybook-addon-react-router-v6';
+import { ITweets } from '../../../types/ITweets';
+import { tweetsMock } from '../../../mockup/Tweets';
+
+export default {
+  title: 'Components/TweetItem',
+  component: TweetBase,
+  decorators: [withRouter],
+  args: {
+    ...tweetsMock[0]
+  }
+} as Meta<ITweets>
+
+export const Default: StoryObj<ITweets> = {}
