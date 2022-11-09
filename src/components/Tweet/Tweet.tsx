ad6cc502db79e1af46d43fb49541e13fb8270440
prmacedo <p.ricardomacedo@hotmail.com>
2022-11-09 11:07:19 -0300

    create like tweet functionality

--- src/components/Tweet/Tweet.tsx
+++ src/components/Tweet/Tweet.tsx
@@ -1,61 +1,103 @@
 import { Heading } from "../Heading/Heading";
 import { Text } from "../Text/Text";
 import { Icon } from "../Icon/Icon";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useData } from "../../context/DataContext/DataContext";
 import { ITweets } from "../../types/ITweets";
 import { IUser } from "../../types/IUser";
 import { useUser } from "../../context/UserContext/UserContext";
+import { useFeed } from "../../context/FeedContext/FeedContext";
 
-function timeDiff(postTime: Date) {
-  const now = new Date();
-  // Do your operations
-  const seconds = (now.getTime() - postTime.getTime()) / 1000;
-
-  let timeDiffString = '';
-
-  if (seconds < 1)
-    timeDiffString = 'now';
-  else if (seconds < 60)
-    timeDiffString = `${seconds.toFixed(0)}s`
-  else if (seconds < 3600)
-    timeDiffString = `${(seconds / 60).toFixed(0)}m`
-  else if (seconds < 3600 * 24)
-    timeDiffString = `${(seconds / (3600)).toFixed(0)}h`
-  else
-    timeDiffString = `${(seconds / (3600 * 24)).toFixed(0)}d`
-
-
-  return timeDiffString;
-}
+import defaultPic from "../../imgs/default-profile-pic.svg";
 
 export function Tweet({ id, userId, time, description, img, likes, comments, retweets, share }: ITweets) {
-  const [ wasLikedByCurrentUser, setWasLikedByCurrentUser ] = useState<boolean>();
+  const [ likeStatus, setLikeStatus ] = useState<'wasLiked' | 'notLiked' | 'justLiked' | 'unliked'>();
+  const [ liked, setLiked ] = useState<boolean>();
 
   const { getUserById } = useData();
   const { user: loggedUser, isLoggedIn } = useUser();
+  const { tweets, setTweets } = useFeed();
 
   const [user, setUser] = useState<IUser>();
 
-  useEffect(() => {
+  function timeDiff(postTime: number) {
+    const now = new Date();
+    const seconds = (now.getTime() - postTime) / 1000;
+
+    let timeDiffString = '';
+
+    if (seconds < 1)
+      timeDiffString = 'now';
+    else if (seconds < 60)
+      timeDiffString = `${seconds.toFixed(0)}s`
+    else if (seconds < 3600)
+      timeDiffString = `${(seconds / 60).toFixed(0)}m`
+    else if (seconds < 3600 * 24)
+      timeDiffString = `${(seconds / (3600)).toFixed(0)}h`
+    else
+      timeDiffString = `${(seconds / (3600 * 24)).toFixed(0)}d`
+
+    return timeDiffString;
+  }
+
+  function handleLike() {
+    if (likeStatus === 'notLiked' || likeStatus === 'unliked') {
+      setLikeStatus('justLiked');
+    } else {
+      setLikeStatus('unliked');
+    }
+  }
+
+  useLayoutEffect(() => {
     setUser(getUserById(userId));
 
-    if(isLoggedIn) {
-      setWasLikedByCurrentUser(likes.find(like => like.userId === loggedUser.id) ? true : false);
+    if(isLoggedIn && likes.find(like => like.userId === loggedUser.id)) {
+      setLikeStatus('wasLiked');
+      setLiked(true);
     } else {
-      setWasLikedByCurrentUser(false);
+      setLikeStatus('notLiked');
+      setLiked(false);
     }
+
+    console.log(tweets);
+
   }, []);
+
+  useEffect(() => {
+    if(likeStatus === 'justLiked') {
+      setLiked(true);
+
+      const updatedTweets = tweets.map(tweet => {
+        if (tweet.id === id) {
+          tweet.likes.push({ userId: loggedUser.id });
+        }
+        return { ...tweet }
+      });
+      setTweets(updatedTweets);
+    } else if(likeStatus === 'unliked') {
+      setLiked(false);
+
+      const updatedTweets = tweets.map(tweet => {
+        if (tweet.id === id) {
+          const likes = tweet.likes.filter(like => like.userId !== loggedUser.id);
+          return { ...tweet, likes }
+        }
+        return { ...tweet }
+      });
+      setTweets(updatedTweets);
+
+    }
+  }, [likeStatus]);
 
   return (
     <div className="tweet flex items-start gap-2.5 pl-4 py-2.5 border-t-2 border-dark-7 dark:border-dark-4">
-      <img src={ user?.profilePic } alt={ user?.name } className="w-12 rounded-full" />
+      <img src={ user?.profilePic || defaultPic } alt={ user?.name || 'Name' } className="w-12 rounded-full" />
 
       <div className="feed__content pr-3 w-full">
         <div className="feed__header flex gap-1 items-center mb-1">
-          <Heading size="xs">{ user?.name }</Heading>
-          <Text color="gray">@{ user?.user }</Text>
+          <Heading size="xs">{ user?.name || 'Name' }</Heading>
+          <Text color="gray">@{ user?.user || 'username' }</Text>
           <Text color="gray">·</Text>
           <Text color="gray">{ timeDiff(time) } </Text>
         </div>
@@ -84,9 +126,9 @@ export function Tweet({ id, userId, time, description, img, likes, comments, ret
           </div>
 
           <div className="feed__iconGroup w-1/4">
-            <div className="cursor-pointer inline-flex gap-2.5"  onClick={() => setWasLikedByCurrentUser(!wasLikedByCurrentUser)}>
-              <Icon size="1.125rem" icon='like' fill={ wasLikedByCurrentUser } color={ wasLikedByCurrentUser ? 'red' : 'gray' } />
-              <Text size="xs" color={ wasLikedByCurrentUser ? 'red' : 'gray' }>{ likes.length }</Text>
+            <div className="cursor-pointer inline-flex gap-2.5"  onClick={() => handleLike()}>
+              <Icon size="1.125rem" icon='like' fill={ liked } color={ liked ? 'red' : 'gray' } />
+              <Text size="xs" color={ liked ? 'red' : 'gray' }>{ likes.length }</Text>
             </div>
           </div>
 
